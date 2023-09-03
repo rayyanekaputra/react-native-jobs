1bce2761e092e20c3a1cddfdf7f73b9ac9dbab17
rayyanekaputra <bullzhit99@gmail.com>
2023-09-03 16:44:06 +0800

    chore: add assets and styles folder

--- app/index.js
+++ app/index.js
@@ -1,11 +1,26 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZE } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBTN,
+  Welcome,
+} from "../components";
+
 
 const Home = () => {
-    return (
-        <View>
-            <Text>Home</Text>
-        </View>
-    )
-}
+    const router = useRouter();
+
+
+
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
 
 export default Home;
